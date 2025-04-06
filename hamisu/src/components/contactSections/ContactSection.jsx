0743d57d0f370d23d29415296f537af68203fe49
Captain_Sani <sani.jdogo@gmail.com>
2025-04-06 18:08:44 +0100

    vercel

--- hamisu/src/components/contactSections/ContactSection.jsx
+++ hamisu/src/components/contactSections/ContactSection.jsx
@@ -1,23 +1,22 @@
 import React from 'react';
 
-
 const ContactSection = () => {
   return (
-    <section className="py-10 pl-24">
+    <section className="py-10 px-4 sm:px-8 md:px-16 lg:px-24">
       <div className="flex flex-col md:flex-row gap-10 items-start md:items-center">
         {/* Contact Info Text */}
-        <div className="md:w-1/3 text-center md:text-left">
-          <p className="font-bold text-4xl">Contact Info</p>
-          <p className="text-4xl mt-2">We are always</p>
-          <p className="text-4xl">
+        <div className="text-center md:text-left">
+          <p className="font-bold text-3xl sm:text-4xl md:text-5xl lg:text-6xl">Contact Info</p>
+          <p className="text-2xl sm:text-3xl mt-2">We are always</p>
+          <p className="text-2xl sm:text-3xl">
             <span className="text-orange-500 font-bold">happy</span> to assist you
           </p>
         </div>
 
         {/* Email Section */}
-        <div className="space-y-3 md:w-1/3">
+        <div className="space-y-3 text-center md:text-left">
           <p className="text-xl font-semibold">Email Address</p>
-          <div className="border border-gray-400 w-16"></div>
+          <div className="border border-gray-400 w-16 mx-auto md:mx-0"></div>
           <span className="block">hamisudantinkiv@gmail.com</span>
           <span className="block">hamisumuhammeddantinki@yahoo.com</span>
           <p className="font-light">Visit any of our branches</p>
@@ -25,15 +24,14 @@ const ContactSection = () => {
         </div>
 
         {/* Phone Section */}
-        <div className="space-y-3 md:w-1/3">
+        <div className="space-y-3 text-center md:text-left">
           <p className="text-xl font-semibold">Number</p>
-          <div className="border border-gray-400 w-16"></div>
+          <div className="border border-gray-400 w-16 mx-auto md:mx-0"></div>
           <span className="block">+234 907 538 7377</span>
           <span className="block">+234 803 349 5555</span>
           <span className="block">+234 803 733 2201</span>
         </div>
       </div>
-     
     </section>
   );
 };
--- hamisu/src/components/contactSections/GetStartedSection.jsx
+++ hamisu/src/components/contactSections/GetStartedSection.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
+import React from 'react';
 
 const GetStartedSection = () => {
   return (
-    <section className='pl-23'>
-        <p className='font-light pb-5 pt-5'>Get Started</p>
-        <div className='text-5xl font-semibold'>
-        <span className='block'>Get in touch with us. We're here to</span>
-        <span className='block'><span className='text-orange-500'>assist</span> you.</span>
-        </div>
-
+    <section className="px-4 sm:px-8 md:px-16 lg:px-24 py-10 text-center md:text-left">
+      <p className="text-lg font-light pb-3 text-gray-600">Get Started</p>
+      <div className="text-3xl sm:text-4xl md:text-5xl font-semibold leading-snug">
+        <span className="block">Get in touch with us. We're here to</span>
+        <span className="block">
+          <span className="text-orange-500">assist</span> you.
+        </span>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default GetStartedSection
+export default GetStartedSection;
